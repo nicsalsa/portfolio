74294c4356bc0c49216c0a72340ad471bc651264
Nicole <ensalced@gmail.com>
2018-09-16 11:20:57 -0400

    added fixed nav and hamburger menu and started working on about me section

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,2 +1,15 @@
-const hello = 'World';
-console.log(hello);
+
+
+// $(window).scroll(function () {
+//    var $heightScrolled = $(window).scrollTop();
+//    var $defaultHeight = 100;
+
+//    if ($heightScrolled < $defaultHeight) {
+//       $('.nav').removeClass("wrapper")
+//       $('.nav').addClass("wrapperNav")
+//    }
+//    // else {
+//    //    $('.nav').addClass("wrapper")
+//    // }
+
+// });
