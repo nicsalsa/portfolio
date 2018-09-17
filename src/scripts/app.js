105b0be47c31ddbaf92c9b31bdd00849b050d31a
Nicole <ensalced@gmail.com>
2018-09-17 08:13:11 -0400

    finished about me section

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -13,3 +13,9 @@
 //    // }
 
 // });
+
+// const $hamburger = $(".hamburger");
+// $hamburger.on("click", function (e) {
+//    $hamburger.toggleClass("is-active");
+//    // Do something else, like open/close menu
+// });
