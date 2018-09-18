6264dc5e810b713d3c67b9c2e10908d8f3c45584
Nicole <ensalced@gmail.com>
2018-09-18 19:25:03 -0400

    worked on some responsive styling

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,40 @@
+// konami code
+const pressed = [];
+const code = "bensbagels";
+// let frameCount = 0;
+
+const finishbagels = () => {
+   let bagel = document.getElementById("bagel-container");
+   bagel.className = "hide";
+}
+
+const trigger = () => {
+   console.log('HAPPENING');
+   setTimeout(
+      
+      function () {
+         finishbagels();
+      }, 10000);
+}
+
+const startbagels = () => {
+   let bagel = document.getElementById("bagel-container");
+   bagel.className = "show";
+   trigger();
+
+}
+
+window.addEventListener("keyup", function (e) {
+   pressed.push(e.key);
+   pressed.splice(-code.length - 1, pressed.length - code.length);
+   console.log(pressed)
+
+   if (pressed.join("").includes(code)) {
+      startbagels();
+   }
+});
+
+
 $(function (){
    $('a').smoothScroll();
-   console.log("read");
 })
-
