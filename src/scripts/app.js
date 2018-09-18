b7614eac2e90cb2f67cbbf2a69e5234e1d635eec
Nicole <ensalced@gmail.com>
2018-09-17 21:43:34 -0400

    worked on 3 portfolio pieces

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,21 +1,5 @@
+$(function (){
+   $('a').smoothScroll();
+   console.log("read");
+})
 
-
-// $(window).scroll(function () {
-//    var $heightScrolled = $(window).scrollTop();
-//    var $defaultHeight = 100;
-
-//    if ($heightScrolled < $defaultHeight) {
-//       $('.nav').removeClass("wrapper")
-//       $('.nav').addClass("wrapperNav")
-//    }
-//    // else {
-//    //    $('.nav').addClass("wrapper")
-//    // }
-
-// });
-
-// const $hamburger = $(".hamburger");
-// $hamburger.on("click", function (e) {
-//    $hamburger.toggleClass("is-active");
-//    // Do something else, like open/close menu
-// });
